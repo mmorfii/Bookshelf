62f6b0c1609f848d8b540251977ae99e03910784
mmorfii <mmorfii.yes@gmail.com>
2023-05-16 15:13:56 +0300

    first commit

---
+++ src/main/java/com/coursework/BookshelfApp/BookshelfAppApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.BookshelfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/BookshelfApp/BookshelfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.BookshelfApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
